e82dd85d64f558757008f6de0c5489689b38cfbe
Jeff Squyres <jsquyres@cisco.com>
2011-02-16 08:53:28 -0500

    Make the version number checking stuff play nice with git

--- config/cci_get_version.sh
+++ config/cci_get_version.sh
@@ -10,13 +10,16 @@
 #                         University of Stuttgart.  All rights reserved.
 # Copyright (c) 2004-2005 The Regents of the University of California.
 #                         All rights reserved.
-# Copyright © 2008-2010 Cisco Systems, Inc.  All rights reserved.
+# Copyright © 2008-2011 Cisco Systems, Inc.  All rights reserved.
 # $COPYRIGHT$
 #
 # Additional copyrights may follow
 #
 # $HEADER$
 #
+
+# 16 Feb 2010: amended to be a bit more general with respect to
+# repository technologies; use "repo_rev" instead of "svn_r".
 
 # 19 May 2010: this file was copied from r2105 from the hwloc SVN
 # trunk.  The only changes were s/hwloc/cci/ig.
@@ -37,8 +40,8 @@
 #  prefix_MINOR_VERSION
 #  prefix_RELEASE_VERSION
 #  prefix_GREEK_VERSION
-#  prefix_WANT_SVN
-#  prefix_SVN_R
+#  prefix_WANT_REPO_REV
+#  prefix_REPO_REV
 #  prefix_RELEASE_DATE
 
 srcfile="$1"
@@ -47,10 +50,10 @@ option="$2"
 case "$option" in
     # svnversion can take a while to run.  If we don't need it, don't run it.
     --major|--minor|--release|--greek|--base|--help)
-        ompi_ver_need_svn=0
+        ompi_ver_need_repo_rev=0
         ;;
     *)
-        ompi_ver_need_svn=1
+        ompi_ver_need_repo_rev=1
 esac
 
 
@@ -58,8 +61,10 @@ if test -z "$srcfile"; then
     option="--help"
 else
 
-    : ${ompi_ver_need_svn=1}
+    : ${ompi_ver_need_repo_rev=1}
     : ${srcdir=.}
+    # Some people like to override svnversion because it takes a long
+    # time on networked filesystems with a big checkout.
     : ${svnversion_result=-1}
 
         if test -f "$srcfile"; then
@@ -70,8 +75,8 @@ else
 	s/^minor/CCI_MINOR_VERSION/
 	s/^release/CCI_RELEASE_VERSION/
 	s/^greek/CCI_GREEK_VERSION/
-	s/^want_svn/CCI_WANT_SVN/
-	s/^svn_r/CCI_SVN_R/
+	s/^want_repo_rev/CCI_WANT_REPO_REV/
+	s/^repo_rev/CCI_REPO_REV/
 	s/^date/CCI_RELEASE_DATE/
 	t print
 	b
@@ -88,23 +93,25 @@ else
         CCI_VERSION="${CCI_VERSION}${CCI_GREEK_VERSION}"
         CCI_BASE_VERSION=$CCI_VERSION
 
-        if test $CCI_WANT_SVN -eq 1 && test $ompi_ver_need_svn -eq 1 ; then
+        if test $CCI_WANT_REPO_REV -eq 1 && test $ompi_ver_need_repo_rev -eq 1 ; then
             if test "$svnversion_result" != "-1" ; then
-                CCI_SVN_R=$svnversion_result
+                CCI_REPO_REV=$svnversion_result
             fi
-            if test "$CCI_SVN_R" = "-1" ; then
+            if test "$CCI_REPO_REV" = "-1" ; then
 
                 if test -d "$srcdir/.svn" ; then
-                    CCI_SVN_R=r`svnversion "$srcdir"`
+                    CCI_REPO_REV=r`svnversion "$srcdir"`
                 elif test -d "$srcdir/.hg" ; then
-                    CCI_SVN_R=hg`hg -v -R "$srcdir" tip | grep changeset | cut -d: -f3`
+                    CCI_REPO_REV=hg`hg -v -R "$srcdir" tip | grep changeset | cut -d: -f3`
+                elif test -d "$srcdir/.git" ; then
+                    CCI_REPO_REV=git`git log -1 "$srcdir" | grep commit | awk '{ print $2 }'`
                 fi
-                if test "CCI_SVN_R" = ""; then
-                    CCI_SVN_R=svn`date '+%m%d%Y'`
+                if test "CCI_REPO_REV" = ""; then
+                    CCI_REPO_REV=date`date '+%m%d%Y'`
                 fi
 
             fi
-            CCI_VERSION="${CCI_VERSION}${CCI_SVN_R}"
+            CCI_VERSION="${CCI_VERSION}${CCI_REPO_REV}"
         fi
     fi
 
@@ -130,8 +137,8 @@ case "$option" in
     --greek)
 	echo $CCI_GREEK_VERSION
 	;;
-    --svn)
-	echo $CCI_SVN_R
+    --repo-rev)
+	echo $CCI_REPO_REV
 	;;
     --base)
         echo $CCI_BASE_VERSION
@@ -140,7 +147,7 @@ case "$option" in
         echo $CCI_RELEASE_DATE
         ;;
     --all)
-        echo ${CCI_VERSION} ${CCI_MAJOR_VERSION} ${CCI_MINOR_VERSION} ${CCI_RELEASE_VERSION} ${CCI_GREEK_VERSION} ${CCI_SVN_R}
+        echo ${CCI_VERSION} ${CCI_MAJOR_VERSION} ${CCI_MINOR_VERSION} ${CCI_RELEASE_VERSION} ${CCI_GREEK_VERSION} ${CCI_REPO_REV}
         ;;
     -h|--help)
 	cat <<EOF
