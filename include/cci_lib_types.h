ad280a87b5830bbb73e3597ea165e99aac77b9c6
Scott Atchley <atchleyes@ornl.gov>
2011-02-17 14:07:20 -0500

    Add lib types - these are private structs
    Only include as much as is needed. Each private type
    allows plugins to have private structs as well
    (i.e. struct foo has a void *priv that the plugin can do anything it
    wants with including ignoring)

---
+++ include/cci_lib_types.h
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2010-2011 UT-Battelle, LLC.  All rights reserved.
+ * Copyright (c) 2010-2011 Oak Ridge National Labs.  All rights reserved.
+ *
+ * See COPYING in top-level directory
+ *
+ * $COPYRIGHT$
+ *
+ * Private data structures for the Common Communications Interface (CCI).
+ */
+
+#include <pthread.h>
+#include "bsd/queue.h"
+
+/* NOTE: struct naming scheme
+ *       - Private structs start with cci__
+ *       - Names should not be be the same as the public counterpart
+ *         to avoid accidentally using the public struct or vice versa
+ *         (e.g. cci__device_t)
+ *       - List entry variables are entry if only one per struct
+ *         else they start with the first letter of the struct
+ *         that they will hang on
+ *         (e.g. dentry will hang on a device
+ *               sentry will hang on a service)
+ *       - Public struct field names should be their name
+ *         (e.g. cci_device_t device;)
+ */
+
+/*! CCI private device */
+typedef struct cci__dev {
+    /*! Public device (name, info, argv, max_send_size, rate, pci) */
+    cci_device_t device;
+
+    /*! entry to hang this dev on the globals->devs */
+    TAILQ_ENTRY(cci__dev) entry;
+
+    /*! Listening endpoints */
+    TAILQ_HEAD(s_leps, cci__lep) leps;
+
+    /*! Pointer to device specific struct */
+    void *priv;
+} cci__dev_t;
+
+/*! CCI private endpoint */
+typedef struct cci__ep {
+    /*! Public endpoint (max_recv_buffer_count) */
+    cci_endpoint_t endpoint;
+
+    /*! Max header size for txs and RMAs */
+    uint8_t max_hdr_size;
+
+    /*! Number of rx buffers */
+    uint32_t rx_buf_cnt;
+
+    /*! Number of tx buffers */
+    uint32_t tx_buf_cnt;
+
+    /*! Size of rx/tx buffers */
+    uint32_t buffer_len;
+
+    /*! Send timeout in microseconds */
+    uint32_t tx_timeout;
+
+    /*! Events ready for process */
+    TAILQ_HEAD(s_evts, cci__evt) evts;
+
+    /*! Lock to protect evts */
+    pthread_mutex_t lock;
+
+    /*! Owning dev */
+    cci__dev_t *dev;
+
+    /*! Entry to hang on dev->eps */
+    TAILQ_ENTRY(cci__ep) entry;
+
+    /*! List to hold open connections */
+    TAILQ_HEAD(s_conns, cci__conn) conns;
+
+    /*! Pointer to device specific struct */
+    void *priv;
+} cci__ep_t;
+
+/*! CCI private connection request */
+typedef struct cci__crq {
+    /*! Public connection request (devices, cnt, ptr, len, attribute) */
+    cci_conn_req_t    conn_req;
+
+    /*! Entry to hang on lep->crqs and svc->crqs */
+    TAILQ_ENTRY(cci__crq) entry;
+
+    /*! Pointer to device specific struct */
+    void *priv;
+} cci__crq_t;
+
+/*! CCI private connection */
+typedef struct cci__conn {
+    /*! Public connection (max_send_size, endpoint, attribute) */
+    cci_connection_t connection;
+
+    /*! URI we connected to if we called connect */
+    const char *uri;
+
+    /*! Send timeout in microseconds (if 0 use ep->tx_timeout) */
+    uint32_t tx_timeout;
+
+    /*! Entry to hang on ep->conns */
+    TAILQ_ENTRY(cci__conn) entry;
+
+    /*! Pointer to device specific struct */
+    void *priv;
+} cci__conn_t;
+
+/*! CCI private connection manager service */
+typedef struct cci__svc {
+    /*! Public service (bogus) */
+    cci_service_t service;
+
+    /*! Port to listen on */
+    uint32_t port;
+
+    /*! Bound listening endpoints */
+    TAILQ_HEAD(s_leps, cci__lep) leps;
+
+    /*! Pending connection requests */
+    TAILQ_HEAD(s_crqs, cci__crq) crqs;
+
+    /*! Lock to protect leps and crqs */
+    pthread_mutex_t lock;
+
+    /* Entry to hang on globals->svcs */
+    TAILQ_ENTRY(cci__svc) entry;
+} cci__svc_t;
+
+/*! CCI private event */
+typedef struct cci__evt {
+    /*! Public event (type, union of send/recv/other) */
+    cci_event_t event;
+
+    /*! Owning endpoint */
+    cci__ep_t *ep;
+
+    /*! Entry to hang on ep->evts */
+    TAILQ_ENTRY(cci__evt) entry;
+
+    /*! Pointer to device specific struct */
+    void *priv;
+} cci__evt_t;
+
+/*! CCI private listening endpoint (created when device is bound to service) */
+typedef struct cci__lep {
+    /*! Owning (bound) device */
+    struct cci__dev *dev;
+
+    /*! Service we are bound to */
+    struct cci__svc *svc;
+
+    /*! Entry to hang on svc->leps */
+    TAILQ_ENTRY(cci__lep) sentry;
+
+    /*! Entry to hang on dev->leps */
+    TAILQ_ENTRY(cci__lep) dentry;
+
+    /*! List of idle connection requests */
+    TAILQ_HEAD(s_crqs, cci__crq) crqs;
+
+    /*! Lock to protect crqs */
+    pthread_mutex_t lock;
+
+    /*! Pointer to device specific struct */
+    void *priv;
+} cci__lep_t;
+
+/*! CCI private global state */
+typedef struct cci__globals {
+    /*! List of all know devices */
+    TAILQ_HEAD(s_devs, cci__dev) devs;
+
+    /*! Array of user devices */
+    cci_device_t **devices;
+
+    /*! Lock to protect svcs */
+    pthread_mutex_lock lock;
+
+    /*! List of connection manager services sorted by port number */
+    TAILQ_HEAD(s_svcs, cci__svc) svcs;
+} cci__globals_t;
+
+extern cci__globals_t globals;
+
+/*! Obtain the private struct from the public struct
+ *  Example 1:
+ *    cci_endpoint_t *endpt;
+ *    cci__ep_t *ep;
+ *
+ *    ep = container_of(endpt, cci__ep, endpoint);
+ *
+ *  Example 2:
+ *    cci_device_t *device;
+ *    cci__dev_t *dev;
+ *
+ *    dev = container_of(device, cci__dev, device);
+ *
+ *    where the first use of "device" is the variable
+ *    the "cci__dev" is the parent struct
+ *    and the second device is the name of the field in the parent struct
+ *
+ *    If we always use the name of the field in the parent struct for
+ *    the local variable name, then the name is repeated as in
+ *    example 2 */
+#define container_of(p, stype, field) ((stype *)(((uint8_t *)(p)) - offsetof(stype, field)))
