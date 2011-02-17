d0b10f402e87368265610e1da6b324c20a77d1ef
Scott Atchley <atchleyes@ornl.gov>
2011-02-17 14:05:12 -0500

    Init can be called more than once
    Allow for it but don't actually support subsequent calls

--- src/api/init.c
+++ src/api/init.c
@@ -12,10 +12,12 @@
 #include "plugins/core/core.h"
 #include "plugins/core/base/public.h"
 
+cci__globals_t *globals = NULL;
 
 int cci_init(uint32_t abi_ver, uint32_t flags, uint32_t *caps)
 {
     int ret;
+    static int once = 0;
 
     if (abi_ver != CCI_ABI_VERSION) {
         fprintf(stderr, "cci_init: got ABI version %d, but expected %d\n",
@@ -23,14 +25,27 @@ int cci_init(uint32_t abi_ver, uint32_t flags, uint32_t *caps)
         return CCI_EINVAL;
     }
 
-    if (CCI_SUCCESS != (ret = cci_plugins_init())) {
-        return ret;
-    }
-    if (CCI_SUCCESS != (ret = cci_plugins_core_open())) {
-        return ret;
-    }
-    if (NULL == cci_core) {
-        return CCI_ERR_NOT_FOUND;
+    if (0 == once) {
+        once++;
+
+        if (CCI_SUCCESS != (ret = cci_plugins_init())) {
+            return ret;
+        }
+        if (CCI_SUCCESS != (ret = cci_plugins_core_open())) {
+            return ret;
+        }
+        if (NULL == cci_core) {
+            return CCI_ERR_NOT_FOUND;
+        }
+    } else {
+        /* TODO */
+        /* check parameters */
+        /* if same, this is a no-op and return SUCCESS */
+        /* if different, can we accomodate new params?
+         *    if yes, do so and return SUCCESS
+         *    if not, ignore and return CCI_ERROR
+         */
+        return CCI_ERR_NOT_IMPLEMENTED;
     }
 
     return cci_core->init(abi_ver, flags, caps);
