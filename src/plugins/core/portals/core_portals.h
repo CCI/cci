e8d783812eeccd647808a4e168dd68fae3ea2c39
Kenneth D. Matney, Sr <matneykdsr@ornl.gov>
2011-03-29 09:11:54 -0400

    Next step in device driver development.

--- src/plugins/core/portals/core_portals.h
+++ src/plugins/core/portals/core_portals.h
@@ -12,31 +12,148 @@
 
 BEGIN_C_DECLS
 
-#define PORTALS_PROG_TIME_US       (10000) /* try to progress every N microseconds */
-#define PORTALS_RESEND_TIME_SEC    (1)     /* time between resends in seconds */
+#define PORTALS_EP_MAX_HDR_SIZE    (8)       /* bytes */
+#define PORTALS_BLOCK_SIZE         (64)      /* bytes for id storage */
+#define PORTALS_EP_BUF_LEN         (8192)    /* 8 kB for now */
+#define PORTALS_EP_TX_TIMEOUT_SEC  (60)      /* seconds for now */
+#define PORTALS_EP_HASH_SIZE       (256)     /* nice round number */
+#define PORTALS_NUM_BLOCKS         (16384)   /* number of blocks */
+#define PORTALS_CONN_REQ_HDR_LEN   ((int) (sizeof(struct portals_header_r)))
+#define PORTALS_RESEND_TIME_SEC    (1)       /* time between resends in seconds */
+#define PORTALS_PROG_TIME_US       (10000)   /* try to progress every N microseconds */
 #define PORTALS_RESEND_CYCLES      (PORTALS_RESEND_TIME_SEC * 1000000 / PORTALS_PROG_TIME_US)
+
+typedef enum portals_msg_type {
+
+    PORTALS_MSG_INVALID=0,
+    PORTALS_MSG_CONN_REQUEST,                /* SYN */
+    PORTALS_MSG_CONN_REPLY,                  /* SYN-ACK */
+    PORTALS_MSG_CONN_ACK,                    /* ACK */
+    PORTALS_MSG_DISCONNECT,                  /* no disconnect is sent */
+    PORTALS_MSG_SEND,
+    PORTALS_MSG_KEEPALIVE,
+
+    /* the rest apply to reliable connections only */
+    PORTALS_MSG_ACK_ONLY,                    /* ack only this seqno */
+    PORTALS_MSG_ACK_UP_TO,                   /* ack up to and including this seqno */
+    PORTALS_MSG_SACK,                        /* ack these blocks of sequences */
+    PORTALS_MSG_RMA_WRITE,
+    PORTALS_MSG_RMA_WRITE_DONE,
+    PORTALS_MSG_RMA_READ_REQUEST,
+    PORTALS_MSG_RMA_READ_REPLY,
+    PORTALS_MSG_TYPE_MAX
+} portals_msg_type_t;
+
+typedef enum portals_tx_state_t {
+
+    PORTALS_TX_IDLE=0,                       /* available */
+    PORTALS_TX_QUEUED,                       /* queued for sending */
+    PORTALS_TX_PENDING,                      /* sent, waiting ack */
+    PORTALS_TX_COMPLETED                     /* completed, status set */
+} portals_tx_state_t;
+
+typedef struct portals_tx {
+
+    cci__evt_t evt;                          /* associated event */
+    portals_msg_type_t msg_type;             /* message type */
+    int flags;                               /* (CCI_FLAG_[BLOCKING|SILENT|NO_COPY]) */
+    portals_tx_state_t state;                /* state of send */
+    void *buffer;                            /* Buffer */
+    uint16_t len;                            /* Buffer length */
+    /*! Entry for hanging on ep->idle_txs, dev->queued, dev->pending */
+    TAILQ_ENTRY(portals_tx) dentry;
+    /*! Entry for hanging on ep->txs */
+    TAILQ_ENTRY(portals_tx) tentry;
+
+    /*! If reliable, use the following: */
+    uint64_t seq;                            /* Sequence number */
+    uint64_t last_attempt_us;                /* Last send (usec) */
+    uint64_t timeout_us;                     /* Timeout (usec) */
+} portals_tx_t;
+
+typedef struct portals_rx {
+
+    cci__evt_t evt;                          /* associated event */
+    void *buffer;                            /* Buffer */
+    uint16_t len;                            /* Buffer length */
+    /*! Entry for hanging on ep->idle_rxs, ep->loaned */
+    TAILQ_ENTRY(portals_rx) entry;
+    /*! Entry for hanging on ep->rxs */
+    TAILQ_ENTRY(portals_rx) gentry;
+} portals_rx_t;
+
+typedef struct portals_seq_ack {
+
+    uint32_t seq;
+    uint32_t seq_ack;
+    uint32_t ack;
+} portals_seq_ack_t;
+
+typedef struct portals__header {
+    uint32_t type;  /* upper 8b are type, next 8b are A and rest are B */
+    uint32_t c;
+} portals_header_t;
+
+typedef struct portals_header_r {             /* reliable header */
+
+    portals_header_t header;
+    portals_seq_ack_t seq_ack;
+} portals_header_r_t;
+
 
 typedef struct portals_dev {
 
     ptl_process_id_t                  idp;
-    TAILQ_HEAD(p_queued, portals_tx)  queued;  /*! Queued sends */
-    TAILQ_HEAD(p_pending, portals_tx) pending; /*! Pending sends */
-    pthread_mutex_t                   lock;    /*! For queued/pending */
-    int                        is_progressing; /*! Being progressed? */
+    /* portals limits imposed by installation */
+    int                max_mes;              /* Max Match Entries */
+    int                max_mds;              /* Max Memory Descriptors */
+    int                max_eqs;              /* Max Event Queues */
+    int                max_ac_index;         /* Max Access Control Table */
+    int                max_pt_index;         /* Max Portals Table Index */
+    int                max_md_iovecs;        /* Max Number of IO vectors */
+    int                max_me_list;          /* Max ME's to Portals Index */
+    int                max_getput_md;        /* Max length of atomic swap */
+    TAILQ_HEAD(p_queued, portals_tx)  queued;  /* Queued sends */
+    TAILQ_HEAD(p_pending, portals_tx) pending; /* Pending sends */
+    pthread_mutex_t                   lock;  /* For queued/pending */
+    int                is_progressing;       /* Being progressed? */
 
 } portals_dev_t;
 
 typedef struct portals_globals {
 
-    int count;                      /* Number of portals devices */
-    const cci_device_t **devices;   /* Array of portals devices */
-    int                shutdown;    /* In shutdown? */
+    int                count;                /* Number of portals devices */
+    const cci_device_t **devices;            /* Array of portals devices */
+    int                shutdown;             /* In shutdown? */
 } portals_globals_t;
 extern portals_globals_t *pglobals;
 
+typedef struct portals_ep {
+
+    TAILQ_HEAD(p_txs, portals_tx) txs;       /* List of all txs */
+    TAILQ_HEAD(p_txsi, portals_tx) idle_txs; /* List of idle txs */
+    TAILQ_HEAD(p_rxs, portals_rx) rxs;       /* List of all rxs */
+    TAILQ_HEAD(p_rxsi, portals_rx) idle_rxs; /* List of idle rxs */
+    pthread_mutex_t lock;                    /* Lock for lists */
+    uint64_t *ids;                           /* Connection id blocks */
+} portals_ep_t;
+
+typedef struct portals_lep {
+
+    cci_os_handle_t fd;                      /* OS handle for poll */
+} portals_lep_t;
+
+typedef struct portals_crq {
+
+    void *buffer;                            /* Buffer for request */
+    ptl_process_id_t idp;                    /* Client's address */
+    uint32_t peer_id;                        /* Peer's id if rejected */
+    uint64_t last_attempt_us;                /* Last send (usec) if reject */
+    uint64_t timeout_us;                     /* Timeout (usec) if reject */
+} portals_crq_t;
+
 int cci_core_portals_post_load(cci_plugin_t *me);
 int cci_core_portals_pre_unload(cci_plugin_t *me);
-
 END_C_DECLS
 
 #endif /* CCI_CORE_PORTALS_H */
