f08fb95d840501a13cf53c223a18ab09a0d19803
Scott Atchley <atchleyes@ornl.gov>
2011-05-31 14:38:28 -0400

    examples: add code exmaples and enable in Doxyfile
    Then reference the examples in cci.h to add to documentation

---
+++ examples/init.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "cci.h"
+
+int main(int argc, char *argv[])
+{
+    int ret;
+    uint32_t caps;
+
+    ret = cci_init(CCI_ABI_VERSION, 0, &caps);
+    if (ret != CCI_SUCCESS)
+        fprintf(stderr, "cci_init() returned %s\n", cci_strerror(ret));
+
+    return 0;
+}
