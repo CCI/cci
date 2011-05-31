fd3d1fc40c7fb8f8dac370b0a41a029d1a7e96b1
Scott Atchley <atchleyes@ornl.gov>
2011-05-31 16:39:27 -0400

    Check that endpoint is bound to conn_req device
    Also, remove incorrect check and printf

--- src/api/accept.c
+++ src/api/accept.c
@@ -20,11 +20,12 @@
 int cci_accept(cci_conn_req_t *conn_req, cci_endpoint_t *endpoint, 
                cci_connection_t **connection)
 {
-    int ret;
+    int ret, found = 0;
     cci__ep_t *ep;
     cci__crq_t *crq;
     cci__lep_t *lep;
     cci__dev_t *dev;
+    cci_device_t **device;
 
     if (NULL == conn_req ||
         NULL == endpoint ||
@@ -34,18 +35,23 @@ int cci_accept(cci_conn_req_t *conn_req, cci_endpoint_t *endpoint,
 
     ep = container_of(endpoint, cci__ep_t, endpoint);
     dev = ep->dev;
+
+    /* Check that endpoint is bound to one of the devices in conn_req */
+    for (device = (cci_device_t **) conn_req->devices; *device != NULL; device++) {
+        cci__dev_t *d = container_of(*device, cci__dev_t, device);
+        if (d == dev) {
+            found = 1;
+            break;
+        }
+    }
+    if (!found)
+        return CCI_EINVAL;
+
     crq = container_of(conn_req, cci__crq_t, conn_req);
     lep = crq->lep;
-    if (ep->dev != lep->dev) {
-        /* FIXME
-         * now what? is the conn request stale or still valid?
-         * do we try to clean up (i.e. return the crq to the lep?
-         */
-        return CCI_EINVAL;
-    }
 
-fprintf( stderr, "calling portals_accept\n" );
     ret = cci_core->accept(conn_req, endpoint, connection);
+    /* FIXME check return value */
 
     /* queue crq */
     pthread_mutex_lock(&lep->lock);
