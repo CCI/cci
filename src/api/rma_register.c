f80eaf1bfce27d6bc566c15260f3e63ec68c0f7c
Scott Atchley <atchleyes@ornl.gov>
2011-06-03 12:27:21 -0400

    Check for reliable connection when registering

--- src/api/rma_register.c
+++ src/api/rma_register.c
@@ -22,11 +22,22 @@ int cci_rma_register(cci_endpoint_t *endpoint,
                      void *start, uint64_t length,
                      uint64_t *rma_handle)
 {
+    cci__conn_t *conn = NULL;
+
     if (NULL == endpoint ||
         NULL == rma_handle ||
         NULL == start ||
         0ULL == length) {
         return CCI_EINVAL;
+    }
+
+    if (connection) {
+        conn = container_of(connection, cci__conn_t, connection);
+        if (!cci_conn_is_reliable(conn)) {
+            debug(CCI_DB_INFO, "%s: RMA registration requires a reliable "
+                               "connection", __func__);
+            return CCI_EINVAL;
+        }
     }
 
     return cci_core->rma_register(endpoint, connection, start, length, rma_handle);
