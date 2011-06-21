bdc2a30a5ff1740d35722bcc855451acc207ad03
Scott Atchley <atchley@myri.com>
2011-06-21 13:29:15 -0400

    mx: initial code
    Includes header and init, get_devices, free_devices

--- src/plugins/core/mx/core_mx.h
+++ src/plugins/core/mx/core_mx.h
@@ -10,6 +10,235 @@
 
 BEGIN_C_DECLS
 
+#define MX_MSS              (1024)
+
+#define MX_EP_MAX_HDR_SIZE  (32)
+#define MX_EP_BUF_LEN       (MX_MSS)
+#define MX_EP_RX_CNT        (1024)      /* max rx messages */
+#define MX_EP_TX_CNT        (128)       /* max tx messages */
+
+/* Limit of 4 message types to ensure that we only use 2 bits for msg type */
+typedef enum mx_msg_type {
+    MX_MSG_SEND,
+    MX_MSG_RMA_WRITE,
+    MX_MSG_RMA_READ,
+    MX_MSG_OOB
+}   mx_msg_type_t;
+
+/* generic match bits:
+
+    <--------------------------- 62 bits ------------------------> 2b
+   +--------------------------------------------------------------+--+
+   |                               A                              |T |
+   +--------------------------------------------------------------+--+
+
+   where each message type decides how to use A and T is msg type
+
+ */
+
+#define MX_TYPE_BITS            (2)
+#define MX_TYPE_MASK            ((1ULL << MX_TYPE_BITS) - 1)
+
+#define MX_A_BITS               (62)
+#define MX_A_SHIFT              (2)
+#define MX_A_MASK               (((1ULL << MX_A_BITS) - 1) << MX_A_SHIFT)
+#define MX_A_MAX                (MX_A_MASK)
+
+#define MX_TYPE(x)              ((uint32_t)(((uint64_t)(x)) & MX_TYPE_MASK))
+#define MX_A(x)                 (((uint64_t)(x)) & MX_A_MASK)
+
+#define MX_PACK_BITS(type,a)    ((((uint64_t)(a)) & MX_A_MASK) | \
+                                 (((uint64_t)(type)) & MX_TYPE_MASK))
+
+static inline void
+mx_pack_bits(uint64_t *match_bits, mx_msg_type_t type, uint64_t a)
+{
+    *match_bits = MX_PACK_BITS(type, a);
+}
+
+static inline void
+mx_parse_match_bits(uint64_t match_bits, mx_msg_type_t *type, uint64_t *a)
+{
+    *type = MX_TYPE(match_bits);
+    *a = MX_A(match_bits);
+}
+
+/* Send
+
+   Matchbits:
+    <---------------------------- 64 bits --------------------------->
+    <----------- 32b -------------->  5b  <---- 16b ----> <- 8b -> 1 2b
+   +--------------------------------+----+---------------+--------+-+--+
+   |      receiver endpoint id      |hlen|   data len    | reservd|R|T |
+   +--------------------------------+----+---------------+--------+-+--+
+   where T is MX_MSG_SEND
+
+   R is 0 for UU and 1 for RO/RU
+
+   hdr_data is the receiver's conn opaque handle
+
+ */
+
+typedef enum mx_msg_oob_type {
+    MX_MSG_OOB_CONN_REQUEST = 0x1,
+    MX_MSG_OOB_CONN_REPLY = 0x2,
+    MX_MSG_KEEPALIVE = 0x3
+} mx_msg_oob_type_t;
+
+/* OOB msg payload is a minimum of one uint32_t with the OOB msg type */
+
+/* connection request:
+
+   Matchbits:
+    <--------------------------- 62 bits -------------------------> 2b
+    <------------ 32b ------------> <---- 16b ----> <- 8b -> <4b> 2b 2b
+   +-------------------------------+---------------+--------+----+--+--+
+   |      server endpoint id       |      len      |  attr  |rsv |O |T |
+   +-------------------------------+---------------+--------+----+--+--+
+   where T is MX_MSG_OOB and O is MX_MSG_OOB_CONN_REQUEST
+
+   hdr_data is the client's conn opaque handle
+
+   Payload:
+    <------------- 32b ------------>
+   +--------------------------------+
+   |      max_recv_buffer_count     |
+   +--------------------------------+
+   |      client's endpoint id      |
+   +--------------------------------+
+
+ */
+
+typedef struct mx_conn_request {
+    uint32_t max_send_size;         /* mss that the client supports */
+    uint32_t max_recv_buffer_count; /* max recvs the client can handle */
+    uint32_t client_ep_id;          /* client's endpoint id */
+} mx_conn_request_t;
+
+/* connection reply (accept):
+
+   Matchbits:
+    <--------------------------- 62 bits ----------------------> 2b 2b
+   +-------------------------------+----------------------------+--+--+
+   |      client endpoint id       |           reserved         |O |T |
+   +-------------------------------+----------------------------+--+--+
+   where T is MX_MSG_OOB and O is MX_MSG_OOB_CONN_REPLY
+
+   hdr_data is the client's conn opaque handle
+
+   Payload:
+    <------------- 32b ------------>
+   +--------------------------------+
+   |          max_send_size         |
+   +--------------------------------+
+   |      max_recv_buffer_count     |
+   +--------------------------------+
+   |     server conn opaque upper   |
+   +--------------------------------+
+   |     server conn opaque lower   |
+   +--------------------------------+
+
+ */
+
+typedef struct mx_conn_accept {
+    uint32_t server_ep_id;
+    uint32_t max_recv_buffer_count; /* max recvs the server can handle */
+    uint32_t server_conn_upper;     /* upper 32 bits of server conn opaque */
+    uint32_t server_conn_lower;     /* lower 32 bits of server conn opaque */
+} mx_conn_accept_t;
+
+/* connection reply (reject):
+
+   Matchbits:
+    <--------------------------- 62 bits ----------------------> 2b 2b
+   +-------------------------------+------------------------------+--+
+   |      client endpoint id       |           reserved         |O |T |
+   +-------------------------------+----------------------------+--+--+
+   where T is MX_MSG_OOB and O is MX_MSG_OOB_CONN_REPLY
+
+   hdr_data is the client's conn opaque handle
+
+   NO Payload
+
+ */
+
+typedef struct mx_tx {
+    cci__evt_t          evt;        /* associated event */
+    mx_msg_type_t       msg_type;   /* message type */
+    mx_msg_oob_type_t   oob_type;   /* if MX_MSG_OOB above, set oob type here */
+    int                 flags;      /* (CCI_FLAG_[BLOCKING|SILENT|NO_COPY]) */
+    void                *buffer;    /* active msg buffer */
+    uint16_t            len;        /* length of buffer */
+    TAILQ_ENTRY(mx_tx)  dentry;     /* Hangs on ep->idle_txs  dev->queued */
+                                    /*   dev->pending */
+    TAILQ_ENTRY(mx_tx)  tentry;     /* Hangs on ep->txs */
+} mx_tx_t;
+
+typedef struct mx_rx {
+    cci__evt_t          evt;        /* associated event */
+    void                *buffer;    /* active msg buffer */
+    uint64_t            match;      /* match bits for msg */
+    TAILQ_ENTRY(mx_rx)  entry;      /* Hangs on ep->idle_rxs */
+    TAILQ_ENTRY(mx_rx)  gentry;     /* Hangs on ep->rxs */
+} mx_rx_t;
+
+typedef struct mx_dev {
+    uint32_t            board;          /* board index */
+    int                 is_progressing; /* Being progressed? */
+} mx_dev_t;
+
+typedef struct mx_globals {
+    int                 count;      /* mx devices */
+    const cci_device_t  **devices;  /* Array of devices */
+} mx_globals_t;
+
+extern mx_globals_t *pglobals;
+
+typedef struct mx_ep {
+    uint32_t                        id;         /* id for endpoint multiplexing */
+    int                             in_use;     /* token to serialize get_event */
+    TAILQ_HEAD(p_txs, mx_tx)        txs;        /* List of all txs */
+    TAILQ_HEAD(p_txsi, mx_tx)       idle_txs;   /* List of idle txs */
+    TAILQ_HEAD(p_rxs, mx_rx)        rxs;        /* List of all rxs */
+    TAILQ_HEAD(p_rxsi, mx_rx)       idle_rxs;   /* List of all rxs */
+    TAILQ_HEAD(p_conns, mx_conn)    conns;      /* List of all conns for cleanup */
+} mx_ep_t;
+
+typedef struct mx_lep {
+    cci_os_handle_t     fd;         /* OS handle for poll */
+} mx_lep_t;
+
+typedef struct mx_crq {
+    void                   *buffer;     /* Buffer for optional payload */
+    mx_endpoint_addr_t     epa;         /* Client's endpoint addr */
+    uint32_t               client_id;   /* Client's ep id */
+    uint32_t               max_recv_buffer_count;
+    uint64_t               client_conn; /* Client's conn addr */
+} mx_crq_t;
+
+/* Connection info */
+typedef enum mx_conn_status {
+    MX_CONN_CLOSED = -2,                /* Shutdown */
+    MX_CONN_CLOSING = -1,               /* Disconnect called */
+    MX_CONN_INIT = 0,                   /* NULL (initial) state */
+    MX_CONN_ACTIVE,                     /* Waiting on server ACK */
+    MX_CONN_PASSIVE,                    /* Waiting on client ACK */
+    MX_CONN_READY                       /* Open and usable */
+} mx_conn_status_t;
+
+typedef struct mx_conn {
+    cci__conn_t             *conn;          /* Owning conn */
+    mx_conn_status_t        status;         /* Status */
+    mx_endpoint_addr_t      epa;            /* Peer's (NID, PID) */
+    uint64_t                peer_conn;      /* Peer's conn addr */
+    uint32_t                peer_ep_id;     /* Peer's endpoint ID */
+    uint32_t                max_tx_cnt;     /* Max sends in flight */
+    mx_tx_t                 *tx;            /* for conn request */
+    TAILQ_ENTRY(mx_conn) entry;             /* Hangs on pep->conns */
+} mx_conn_t;
+
+
+
 int cci_core_mx_post_load(cci_plugin_t *me);
 int cci_core_mx_pre_unload(cci_plugin_t *me);
 
--- src/plugins/core/mx/core_mx_api.c
+++ src/plugins/core/mx/core_mx_api.c
@@ -11,6 +11,8 @@
 #include "plugins/core/core.h"
 #include "core_mx.h"
 
+volatile int mx_shut_down = 0;
+mx_globals_t *mglobals = NULL;
 
 /*
  * Local functions
@@ -19,59 +21,59 @@ static int mx_init(uint32_t abi_ver, uint32_t flags, uint32_t *caps);
 static const char *mx_strerror(enum cci_status status);
 static int mx_get_devices(cci_device_t const ***devices);
 static int mx_free_devices(cci_device_t const **devices);
-static int mx_create_endpoint(cci_device_t *device, 
-                                    int flags, 
-                                    cci_endpoint_t **endpoint, 
+static int mx_create_endpoint(cci_device_t *device,
+                                    int flags,
+                                    cci_endpoint_t **endpoint,
                                     cci_os_handle_t *fd);
 static int mx_destroy_endpoint(cci_endpoint_t *endpoint);
-static int mx_bind(cci_device_t *device, int backlog, uint32_t *port, 
+static int mx_bind(cci_device_t *device, int backlog, uint32_t *port,
                          cci_service_t **service, cci_os_handle_t *fd);
 static int mx_unbind(cci_service_t *service, cci_device_t *device);
-static int mx_get_conn_req(cci_service_t *service, 
+static int mx_get_conn_req(cci_service_t *service,
                                  cci_conn_req_t **conn_req);
-static int mx_accept(cci_conn_req_t *conn_req, 
-                           cci_endpoint_t *endpoint, 
+static int mx_accept(cci_conn_req_t *conn_req,
+                           cci_endpoint_t *endpoint,
                            cci_connection_t **connection);
 static int mx_reject(cci_conn_req_t *conn_req);
-static int mx_connect(cci_endpoint_t *endpoint, char *server_uri, 
+static int mx_connect(cci_endpoint_t *endpoint, char *server_uri,
                             uint32_t port,
-                            void *data_ptr, uint32_t data_len, 
+                            void *data_ptr, uint32_t data_len,
                             cci_conn_attribute_t attribute,
-                            void *context, int flags, 
+                            void *context, int flags,
                             struct timeval *timeout);
 static int mx_disconnect(cci_connection_t *connection);
-static int mx_set_opt(cci_opt_handle_t *handle, 
-                            cci_opt_level_t level, 
+static int mx_set_opt(cci_opt_handle_t *handle,
+                            cci_opt_level_t level,
                             cci_opt_name_t name, const void* val, int len);
-static int mx_get_opt(cci_opt_handle_t *handle, 
-                            cci_opt_level_t level, 
+static int mx_get_opt(cci_opt_handle_t *handle,
+                            cci_opt_level_t level,
                             cci_opt_name_t name, void** val, int *len);
 static int mx_arm_os_handle(cci_endpoint_t *endpoint, int flags);
-static int mx_get_event(cci_endpoint_t *endpoint, 
+static int mx_get_event(cci_endpoint_t *endpoint,
                               cci_event_t ** const event,
                               uint32_t flags);
-static int mx_return_event(cci_endpoint_t *endpoint, 
+static int mx_return_event(cci_endpoint_t *endpoint,
                                  cci_event_t *event);
-static int mx_send(cci_connection_t *connection, 
-                         void *header_ptr, uint32_t header_len, 
-                         void *data_ptr, uint32_t data_len, 
+static int mx_send(cci_connection_t *connection,
+                         void *header_ptr, uint32_t header_len,
+                         void *data_ptr, uint32_t data_len,
                          void *context, int flags);
-static int mx_sendv(cci_connection_t *connection, 
-                          void *header_ptr, uint32_t header_len, 
+static int mx_sendv(cci_connection_t *connection,
+                          void *header_ptr, uint32_t header_len,
                           struct iovec *data, uint8_t iovcnt,
                           void *context, int flags);
-static int mx_rma_register(cci_endpoint_t *endpoint, 
+static int mx_rma_register(cci_endpoint_t *endpoint,
                            cci_connection_t *connection,
                            void *start, uint64_t length,
                            uint64_t *rma_handle);
-static int mx_rma_register_phys(cci_endpoint_t *endpoint, 
+static int mx_rma_register_phys(cci_endpoint_t *endpoint,
                                 cci_connection_t *connection,
-                                cci_sg_t *sg_list, uint32_t sg_cnt, 
+                                cci_sg_t *sg_list, uint32_t sg_cnt,
                                 uint64_t *rma_handle);
 static int mx_rma_deregister(uint64_t rma_handle);
-static int mx_rma(cci_connection_t *connection, 
-                        void *header_ptr, uint32_t header_len, 
-                        uint64_t local_handle, uint64_t local_offset, 
+static int mx_rma(cci_connection_t *connection,
+                        void *header_ptr, uint32_t header_len,
+                        uint64_t local_handle, uint64_t local_offset,
                         uint64_t remote_handle, uint64_t remote_offset,
                         uint64_t data_len, void *context, int flags);
 
@@ -97,7 +99,7 @@ cci_plugin_core_t cci_core_mx_plugin = {
         "mx",
         CCI_MAJOR_VERSION, CCI_MINOR_VERSION, CCI_RELEASE_VERSION,
         5,
-        
+
         /* Bootstrap function pointers */
         cci_core_mx_post_load,
         cci_core_mx_pre_unload,
@@ -133,35 +135,163 @@ cci_plugin_core_t cci_core_mx_plugin = {
 
 static int mx_init(uint32_t abi_ver, uint32_t flags, uint32_t *caps)
 {
-    printf("In mx_init\n");
-    return CCI_SUCCESS;
+    int             ret;
+    int             init    = 0;
+    cci__dev_t      *dev;
+    cci_device_t    **devices;
+
+    CCI_ENTER;
+
+    ret = mx_init();
+    if (ret != MX_SUCCESS) {
+        debug(CCI_DB_WARN, "mx_init() returned %s", mx_strerror(ret));
+        ret = CCI_ENODEV;
+        goto out;
+    }
+
+    init = 1;
+
+    mglobals = calloc(1, sizeof(*mglobals));
+    if (!mglobals) {
+        ret = CCI_ENOMEM;
+        goto out;
+    }
+
+    devices = calloc(CCI_MAX_DEVICES, sizeof(*mglobals->devices));
+    if (!devices) {
+        ret = CCI_ENOMEM;
+        goto out;
+    }
+
+    /* find devices that we own */
+
+    TAILQ_FOREACH(dev, &globals->devs, entry) {
+        if (0 == strcmp("mx", dev->driver)) {
+            const char **arg;
+            cci_device_t *device;
+            mx_dev_t *mdev;
+
+            device = &dev->device;
+            device->max_send_size = MX_MSS;
+
+            /* TODO determine link rate */
+            device->rate = 10000000000ULL;
+
+            device->pci.domain = -1;    /* per CCI spec */
+            device->pci.bus = -1;       /* per CCI spec */
+            device->pci.dev = -1;       /* per CCI spec */
+            device->pci.func = -1;      /* per CCI spec */
+
+            dev->priv = calloc(1, sizeof(*mdev));
+            if (!dev->priv) {
+                ret = CCI_ENOMEM;
+                goto out;
+            }
+
+            mdev->is_progressing = 0;
+
+            /* parse conf_argv */
+            for (arg = device->conf_argv;
+                 *arg != NULL;
+                 arg++) {
+                if (0 == strncmp("mtu=", *arg, 4)) {
+                    const char *mss_str = *arg + 4;
+                    uint32_t mss = strtol(mss_str, NULL, 0);
+
+                    assert(mss == MX_MSS);
+                    device->max_send_size = mss;
+                }
+            }
+            devices[mglobals->count] = device;
+            mglobals->count++;
+            dev->is_up = 1;
+        }
+    }
+
+    devices = realloc(devices, (mglobals->count + 1) * sizeof(cci_device_t *));
+    devices[mglobals->count] = NULL;
+
+    *((cci_device_t ***) &mglobals->devices) = devices;
+
+out:
+    if (ret) {
+        if (devices) {
+            cci_device_t const *device;
+            cci__dev_t *dev;
+
+            for (device = devices[0];
+                 device != NULL;
+                 device++) {
+                dev = container_of(device, cci__dev_t, device);
+                if (dev->priv)
+                    free(dev->priv);
+            }
+            free(devices);
+        }
+
+        if (mglobals) {
+            free(mglobals);
+            mglobals = NULL;
+        }
+
+        if (init)
+            mx_finalize();
+    }
+
+    CCI_EXIT;
+    return ret;
 }
 
 
 static const char *mx_strerror(enum cci_status status)
 {
-    printf("In mx_sterrror\n");
+    CCI_ENTER;
+
+    CCI_EXIT;
     return NULL;
 }
 
 
 static int mx_get_devices(cci_device_t const ***devices)
 {
-    printf("In mx_get_devices\n");
-    return CCI_ERR_NOT_IMPLEMENTED;
+    CCI_ENTER;
+
+    if (!mglobals) {
+        CCI_EXIT;
+        return CCI_ENODEV;
+    }
+
+    *devices = mglobals->devices;
+
+    CCI_EXIT;
+    return CCI_SUCCESS;
 }
 
 
 static int mx_free_devices(cci_device_t const **devices)
 {
-    printf("In mx_free_devices\n");
-    return CCI_ERR_NOT_IMPLEMENTED;
+    CCI_ENTER;
+
+    pthread_mutex_lock(&globals->lock);
+    mx_shut_down = 1;
+    pthread_mutex_unlock(&globals->lock);
+
+    pthread_mutex_lock(&globals->lock);
+    TAILQ_FOREACH(dev, &globals->devs, entry)
+        free(dev->priv);
+    pthread_mutex_unlock(&globals->lock);
+
+    free(mglobals->devices);
+    free((void *) mglobals);
+
+    CCI_EXIT;
+    return CCI_SUCCESS;
 }
 
 
-static int mx_create_endpoint(cci_device_t *device, 
-                                    int flags, 
-                                    cci_endpoint_t **endpoint, 
+static int mx_create_endpoint(cci_device_t *device,
+                                    int flags,
+                                    cci_endpoint_t **endpoint,
                                     cci_os_handle_t *fd)
 {
     printf("In mx_create_endpoint\n");
@@ -176,7 +306,7 @@ static int mx_destroy_endpoint(cci_endpoint_t *endpoint)
 }
 
 
-static int mx_bind(cci_device_t *device, int backlog, uint32_t *port, 
+static int mx_bind(cci_device_t *device, int backlog, uint32_t *port,
                          cci_service_t **service, cci_os_handle_t *fd)
 {
     printf("In mx_bind\n");
@@ -191,7 +321,7 @@ static int mx_unbind(cci_service_t *service, cci_device_t *device)
 }
 
 
-static int mx_get_conn_req(cci_service_t *service, 
+static int mx_get_conn_req(cci_service_t *service,
                                  cci_conn_req_t **conn_req)
 {
     printf("In mx_get_conn_req\n");
@@ -199,8 +329,8 @@ static int mx_get_conn_req(cci_service_t *service,
 }
 
 
-static int mx_accept(cci_conn_req_t *conn_req, 
-                           cci_endpoint_t *endpoint, 
+static int mx_accept(cci_conn_req_t *conn_req,
+                           cci_endpoint_t *endpoint,
                            cci_connection_t **connection)
 {
     printf("In mx_accept\n");
@@ -215,11 +345,11 @@ static int mx_reject(cci_conn_req_t *conn_req)
 }
 
 
-static int mx_connect(cci_endpoint_t *endpoint, char *server_uri, 
+static int mx_connect(cci_endpoint_t *endpoint, char *server_uri,
                             uint32_t port,
-                            void *data_ptr, uint32_t data_len, 
+                            void *data_ptr, uint32_t data_len,
                             cci_conn_attribute_t attribute,
-                            void *context, int flags, 
+                            void *context, int flags,
                             struct timeval *timeout)
 {
     printf("In mx_connect\n");
@@ -234,8 +364,8 @@ static int mx_disconnect(cci_connection_t *connection)
 }
 
 
-static int mx_set_opt(cci_opt_handle_t *handle, 
-                            cci_opt_level_t level, 
+static int mx_set_opt(cci_opt_handle_t *handle,
+                            cci_opt_level_t level,
                             cci_opt_name_t name, const void* val, int len)
 {
     printf("In mx_set_opt\n");
@@ -243,8 +373,8 @@ static int mx_set_opt(cci_opt_handle_t *handle,
 }
 
 
-static int mx_get_opt(cci_opt_handle_t *handle, 
-                            cci_opt_level_t level, 
+static int mx_get_opt(cci_opt_handle_t *handle,
+                            cci_opt_level_t level,
                             cci_opt_name_t name, void** val, int *len)
 {
     printf("In mx_get_opt\n");
@@ -259,7 +389,7 @@ static int mx_arm_os_handle(cci_endpoint_t *endpoint, int flags)
 }
 
 
-static int mx_get_event(cci_endpoint_t *endpoint, 
+static int mx_get_event(cci_endpoint_t *endpoint,
                               cci_event_t ** const event,
                               uint32_t flags)
 {
@@ -268,7 +398,7 @@ static int mx_get_event(cci_endpoint_t *endpoint,
 }
 
 
-static int mx_return_event(cci_endpoint_t *endpoint, 
+static int mx_return_event(cci_endpoint_t *endpoint,
                                  cci_event_t *event)
 {
     printf("In mx_return_event\n");
@@ -276,9 +406,9 @@ static int mx_return_event(cci_endpoint_t *endpoint,
 }
 
 
-static int mx_send(cci_connection_t *connection, 
-                         void *header_ptr, uint32_t header_len, 
-                         void *data_ptr, uint32_t data_len, 
+static int mx_send(cci_connection_t *connection,
+                         void *header_ptr, uint32_t header_len,
+                         void *data_ptr, uint32_t data_len,
                          void *context, int flags)
 {
     printf("In mx_send\n");
@@ -286,8 +416,8 @@ static int mx_send(cci_connection_t *connection,
 }
 
 
-static int mx_sendv(cci_connection_t *connection, 
-                          void *header_ptr, uint32_t header_len, 
+static int mx_sendv(cci_connection_t *connection,
+                          void *header_ptr, uint32_t header_len,
                           struct iovec *data, uint8_t iovcnt,
                           void *context, int flags)
 {
@@ -306,9 +436,9 @@ static int mx_rma_register(cci_endpoint_t *endpoint,
 }
 
 
-static int mx_rma_register_phys(cci_endpoint_t *endpoint, 
+static int mx_rma_register_phys(cci_endpoint_t *endpoint,
                                 cci_connection_t *connection,
-                                cci_sg_t *sg_list, uint32_t sg_cnt, 
+                                cci_sg_t *sg_list, uint32_t sg_cnt,
                                 uint64_t *rma_handle)
 {
     printf("In mx_rma_register_phys\n");
@@ -323,9 +453,9 @@ static int mx_rma_deregister(uint64_t rma_handle)
 }
 
 
-static int mx_rma(cci_connection_t *connection, 
-                        void *header_ptr, uint32_t header_len, 
-                        uint64_t local_handle, uint64_t local_offset, 
+static int mx_rma(cci_connection_t *connection,
+                        void *header_ptr, uint32_t header_len,
+                        uint64_t local_handle, uint64_t local_offset,
                         uint64_t remote_handle, uint64_t remote_offset,
                         uint64_t data_len, void *context, int flags)
 {
