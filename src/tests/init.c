573ce044db99fb259e4310ceb96e1073bf69fc46
Kenneth D. Matney, Sr <matneykdsr@ornl.gov>
2011-07-08 10:06:07 -0400

    Print error string instead of number

--- src/tests/init.c
+++ src/tests/init.c
@@ -12,8 +12,9 @@ int main(int argc, char* argv[])
 {
     int rc;
 
+/*  This is supposed to fail because we will pass value instead of address. */
     if (CCI_SUCCESS != (rc = cci_init(CCI_ABI_VERSION, 0, 0))) {
-        fprintf(stderr, "Got error back from cci:init: %d\n", rc);
+        fprintf(stderr, "Got error back from cci:init: %s\n", cci_strerror(rc));
         return -1;
     }
 
