4462e9b558c99d8955911e0aaf498c0a7146580d
Brice Goglin <Brice.Goglin@inria.fr>
2011-12-19 09:46:23 +0100

    ccieth: read incoming packet type and endpoint, and check that the interface matches

--- contrib/driver/ccieth/linux/ccieth_recv.c
+++ contrib/driver/ccieth/linux/ccieth_recv.c
@@ -4,11 +4,19 @@
  */
 
 #include <linux/netdevice.h>
+#include <linux/rcupdate.h>
+
+#include <ccieth_common.h>
 
 static int
 ccieth_recv(struct sk_buff *skb, struct net_device *ifp, struct packet_type *pt,
 	    struct net_device *orig_dev)
 {
+	u32 eid;
+	u8 pktype;
+	struct ccieth_endpoint *ep;
+	int err;
+
 	skb = skb_share_check(skb, GFP_ATOMIC);
 	if (unlikely(skb == NULL))
 		return 0;
@@ -16,14 +24,31 @@ ccieth_recv(struct sk_buff *skb, struct net_device *ifp, struct packet_type *pt,
 	/* len doesn't include header */
 	skb_push(skb, ETH_HLEN);
 
-	/* FIXME: find endpoint */
+	/* extract common headers TODO cleanup with an actual struct */
+	err = skb_copy_bits(skb, sizeof(struct ethhdr), &eid, sizeof(eid));
+	if (err)
+		goto out;
+	err = skb_copy_bits(skb, sizeof(struct ethhdr)+sizeof(eid), &pktype, sizeof(pktype));
+	if (err)
+		goto out;
 
-	/* FIXME: check endpoint is attached to this ifp ? */
-
+	/* check endpoint is attached to this ifp */
+	rcu_read_lock();
+	ep = idr_find(&ccieth_ep_idr, eid);
+	rcu_read_unlock();
+	if (!ep || ep->ifp != ifp) {
+		err = -EINVAL;
+		goto out;
+	}
+		
 	printk("got a packet\n");
 
 	dev_kfree_skb(skb);
 	return 0;
+
+out:
+	dev_kfree_skb(skb);
+	return err;
 }
 
 struct packet_type ccieth_pt = {
