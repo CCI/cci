406e39bee883117ab996bdba82607e526765ab9d
Brice Goglin <Brice.Goglin@inria.fr>
2011-12-29 09:38:41 +0100

    ccieth: document the upcoming connection model (if nothing goes bad)

--- contrib/driver/ccieth/linux/ccieth_common.h
+++ contrib/driver/ccieth/linux/ccieth_common.h
@@ -28,11 +28,49 @@ struct ccieth_endpoint_event {
 	struct ccieth_ioctl_get_event event;
 };
 
+/* connection status automata:
+ *
+ * on connect ioctl:
+ * - initiator creates connection with status REQUESTED
+ * - timeout while REQUESTED to resend send_connect
+ *
+ * on incoming connect:
+ * - target creates connection with status RECEIVED, in the receive side to detect duplicates
+ * - the number of received (or total?) connections in limited to avoid ddos
+ * - event only delivered to user-space if we created a connection
+ *
+ * on accept ioctl:
+ * - target switches status to READY
+ * - initiator switches status to READY when receiving accept
+ *
+ * on reject ioctl:
+ * - target switches status to REJECTED
+ * - initiator destroys connection when receiving reject
+ * - initiator sends a reject ack when receiving reject, even if connection was already destroyed
+ * - target destroys connection when receiving reject ack
+ * - timeout while REJECTED to resend send_reject
+ */
+enum ccieth_connection_status {
+	/* status for both sides */
+	CCIETH_CONNECTION_READY,     /* accept sent or received */
+	/* initiator side */
+	CCIETH_CONNECTION_REQUESTED, /* request sent */
+	/* target side */
+	CCIETH_CONNECTION_RECEIVED,  /* request received, not accepted or rejected yet */
+	CCIETH_CONNECTION_REJECTED,  /* reject sent and not acked yet */
+};
+
 struct ccieth_connection {
-	int id;
-	/* FIXME: cache skb headers? */
+	int id; /* always valid */ /* FIXME keep in network order too? */
+	enum ccieth_connection_status status;
+
+	/* dest fields are valid when status RECEIVED, READY or REJECTED */
+	/* FIXME: store in network order? */
+	int dest_id;
 	__u8 dest_addr[6];
 	__u32 dest_eid;
+
+	/* FIXME: cache skb headers? */
 };
 
 extern struct idr ccieth_ep_idr;
