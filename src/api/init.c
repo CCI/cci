7f3fc1f07706bc40cce12bbe11182f6fffd911f3
Scott Atchley <atchleyes@ornl.gov>
2012-06-05 09:53:55 -0400

    API: fix check for ep in CCI_DEBUG
    Use the correct length of "ep" - bad cut and paste

--- src/api/init.c
+++ src/api/init.c
@@ -79,7 +79,7 @@ static inline void cci__get_debug_env(void)
 			mask |= CCI_DB_WARN;
 		} else if (0 == strncmp(debug, "drvr", 4)) {
 			mask |= CCI_DB_DRVR;
-		} else if (0 == strncmp(debug, "ep", 4)) {
+		} else if (0 == strncmp(debug, "ep", 2)) {
 			mask |= CCI_DB_EP;
 		} else if (0 == strncmp(debug, "all", 3)) {
 			mask |= CCI_DB_ALL;
