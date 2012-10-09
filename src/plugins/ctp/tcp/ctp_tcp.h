85211e92e8fa800504f44af535b162326720d3b4
Scott Atchley <atchleyes@ornl.gov>
2012-10-09 11:25:00 -0400

    tcp: update to use cci_rma_handle_t
    Store the address of the tcp_rma_handle_t in the cci_rma_handle_t's
    stuff[0]. Pass this only in the RMA headers. The code path is
    essentially unchanged.

--- src/plugins/ctp/tcp/ctp_tcp.h
+++ src/plugins/ctp/tcp/ctp_tcp.h
@@ -608,6 +608,9 @@ typedef struct tcp_rma_handle {
 	/*! Application memory */
 	void *start;
 
+	/*! CCI RMA handle */
+	cci_rma_handle_t rma_handle;
+
 	/*! Access flags */
 	uint32_t flags;
 
@@ -625,9 +628,9 @@ typedef struct tcp_rma_op {
 	/*! Entry to hang on sconn->rmas */
 	TAILQ_ENTRY(tcp_rma_op) rmas;
 
-	uint64_t local_handle;
+	cci_rma_handle_t * local_handle;
 	uint64_t local_offset;
-	uint64_t remote_handle;
+	cci_rma_handle_t * remote_handle;
 	uint64_t remote_offset;
 
 	uint64_t data_len;
