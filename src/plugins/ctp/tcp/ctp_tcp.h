7e65be7fcbcc5ff6c870c1467c8b31185996367f
Geoffroy Vallee <valleegr@ornl.gov>
2013-05-02 15:28:56 -0400

    Revert "tcp: Improve the management on RX/TX buffers (based on a patch from G. Bosilca)."
    This reverts commit 2a1b8ff5ab4b0ed7754339216df0466ef329f3c6.

--- src/plugins/ctp/tcp/ctp_tcp.h
+++ src/plugins/ctp/tcp/ctp_tcp.h
@@ -703,14 +703,17 @@ typedef struct tcp_ep {
 	/*! Array of conns indexed by fds */
 	cci__conn_t **c;
 
-	/*! Base pointer for the buffers, tcp_rx and tcp_tx allocated */
-	char *tcp_xx_base, *buf_base;
+	/*! TX common buffer */
+	void *tx_buf;
 
 	/*! All txs */
 	tcp_tx_t *txs;
 
 	/*! List of idle txs */
 	TAILQ_HEAD(s_itxs, cci__evt) idle_txs;
+
+	/*! RX common buffer */
+	void *rx_buf;
 
 	/*! All rxs */
 	tcp_rx_t *rxs;
